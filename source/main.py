bf3ce0dffde46f8accb33ead59e2233f46fcfa5e
Samer ABOUJAOUDE <samer.abou-jaoude@telecomnancy.net>
2018-02-26 18:03:40 +0100

    close button

--- source/main.py
+++ source/main.py
@@ -1,14 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
+    screen_res=app.desktop().screenGeometry()
+    width,height=screen_res.width(),screen_res.height()
 
-    w = QWidget()
-    w.resize(500,500)
-    w.move(300, 300)
-    w.setWindowTitle('Projet Anglais avec le point bonus')
-    w.show()
+    window = QWidget()
+    window.resize(300,300)
+    window.move(0,0)
+    window.setWindowTitle('Projet Anglais avec le point bonus')
+    exitButton=QPushButton("Quitter",window)
+    exitButton.clicked.connect(window.close)
+    window.showFullScreen()
     
     sys.exit(app.exec_())
